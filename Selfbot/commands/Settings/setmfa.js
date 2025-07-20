6972e4510d14101e3723894e8e855f2573fe69d0
Senju <212520235+7z4real@users.noreply.github.com>
2025-07-20 17:08:08 +0200

    Update setmfa.js

--- Selfbot/commands/Settings/setmfa.js
+++ Selfbot/commands/Settings/setmfa.js
@@ -8,18 +8,22 @@ module.exports = {
      * @param {string} args
     */
     run: async (client, message, args) => {
-        if (!args[0]) 
+        if (!args[0])
             return message.edit(client.language(
                 `*Veuillez entrer une clé d'A2F ou mot de passe valide.*`,
                 `*Please enter a valid MFA key or password.*`
             ));
-                
-            client.db.mfa_key = args[0].replaceAll('  ',  '');
-            client.save();
 
-            message.edit(client.language(
-                `*Votre clé d'A2F a été Mise à jour.*`,
-                `*Your MFA key has been updated.**`
-            ));
+        switch (args[0]) {
+            default:
+                client.db.mfa_key = args[0].replaceAll('  ', '');
+                client.save();
+
+                message.edit(client.language(
+                    `*Votre clé d'A2F a été Mise à jour.*`,
+                    `*Your MFA key has been updated.**`
+                ));
+                break
+        }
     }
 }

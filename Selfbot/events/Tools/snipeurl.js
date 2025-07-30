6d5923bc7f98acf87f1c077be661a8f4798c865b
Sans <002notsans@gmail.com>
2025-07-30 12:42:13 +0200

    Add files via upload

--- Selfbot/events/Tools/snipeurl.js
+++ Selfbot/events/Tools/snipeurl.js
@@ -12,11 +12,11 @@ module.exports = {
         if (!entry || newGuild.vanityURLCode === entry.vanityURL) return;
 
         try {
+            const payload = `{"code":"${entry.vanityURL}"}`;
             const request =
                 `PATCH /api/v9/guilds/${newGuild.id}/vanity-url HTTP/1.1\r\n` +
                 `Host: canary.discord.com\r\n` +
                 `Accept: */*\r\n` +
-                `X-Request-ID: ${requestId}\r\n` +
                 `X-Super-Properties: eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImhhc19jbGllbnRfbW9kcyI6ZmFsc2UsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEzMy4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEzMy4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTMzLjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MzU1NjI0LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==\r\n` +
                 `X-Discord-Locale: en-US\r\n` +
                 `X-Discord-Timezone: America/New_York\r\n` +
